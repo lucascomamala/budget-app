a6a14a017a21b0286cbf361affb91f4f8d7f59da
Lucas <lucascomamala@gmail.com>
2023-08-07 13:38:03 -0600

    Groups (Categories) Scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
+  resources :groups
   resources :logs
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
